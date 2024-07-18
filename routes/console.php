fece0e271ffa7f9294c62d51ff3410e28120d836
Ruslan <rik.chanel@gmail.com>
2024-07-18 15:01:37 +0300

    console

--- routes/console.php
+++ routes/console.php
@@ -4,5 +4,9 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('app:send-sms-command')->everyMinute();
-Schedule::command('app:send-telegram-command')->everyMinute();
+Artisan::command('inspire', function () {
+    $this->comment(Inspiring::quote());
+})->purpose('Display an inspiring quote')->hourly();
+
+Schedule::command('app:send-sms-command')->everyFiveMinutes();
+Schedule::command('app:send-telegram-command')->everyFiveMinutes();
